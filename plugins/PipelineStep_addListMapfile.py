a0870c52a316317fc8d60732afd847504c9dbfa4
David Rafferty <drafferty@hs.uni-hamburg.de>
2018-11-08 11:52:24 +0100

    Add check that input files exist

--- plugins/PipelineStep_addListMapfile.py
+++ plugins/PipelineStep_addListMapfile.py
@@ -15,6 +15,9 @@ def plugin_main(args, **kwargs):
     hosts : list or str
         List of hosts/nodes. May be given as a list or as a string (e.g.,
         '[host1, host2]'
+    check_files_exist : bool
+        If True, check that the files exists. If a file does not exist, set skip = True in
+        the output mapfile. Default is False
     mapfile_dir : str
         Directory for output mapfile
     filename: str
@@ -43,16 +46,35 @@ def plugin_main(args, **kwargs):
         hosts = [h.strip() for h in hosts]
     mapfile_dir = kwargs['mapfile_dir']
     filename = kwargs['filename']
+    if 'check_files_exist' in kwargs:
+        check_files_exist = string2bool(kwargs['check_files_exist'])
+    else:
+        check_files_exist = False
+
 
     for i in range(len(files)-len(hosts)):
         hosts.append(hosts[i])
 
     map_out = DataMap([])
     for h, f in zip(hosts, files):
-        map_out.data.append(DataProduct(h, f, False))
+        if check_files_exist:
+            skip = not os.path.exists(f)
+        else:
+            skip = False
+        map_out.data.append(DataProduct(h, f, skip))
 
     fileid = os.path.join(mapfile_dir, filename)
     map_out.save(fileid)
     result = {'mapfile': fileid}
 
     return result
+
+def string2bool(instring):
+    if not isinstance(instring, basestring):
+        raise ValueError('string2bool: Input is not a basic string!')
+    if instring.upper() == 'TRUE' or instring == '1':
+        return True
+    elif instring.upper() == 'FALSE' or instring == '0':
+        return False
+    else:
+        raise ValueError('string2bool: Cannot convert string "'+instring+'" to boolean!')
