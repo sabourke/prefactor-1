d28ff2a86df742bd482a7f6e4a0971be2998dbfc
David Rafferty <drafferty@hs.uni-hamburg.de>
2019-02-15 10:19:41 +0100

    Add naming plugin

---
+++ plugins/PipelineStep_getTargetName.py
@@ -0,0 +1,27 @@
+from lofarpipe.support.data_map import DataMap
+import casacore.tables as ct
+
+
+def plugin_main(args, **kwargs):
+    """
+    Finds target name for observation
+
+    Parameters
+    ----------
+    mapfile_in : str
+        Mapfile for input measurement sets
+
+    Returns
+    -------
+    result : dict
+        Output name
+    """
+    mapfile_in = kwargs['mapfile_in']
+    data = DataMap.load(mapfile_in)
+    msfile = data[0]
+
+    observationTable = ct.table(msfile+'::OBSERVATION')
+    targetName = observationTable.getcol('LOFAR_TARGET')['array'][0]
+    result = {'targetName': targetName}
+
+    return result
