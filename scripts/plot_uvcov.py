5ca22c44ca931ba26cb3f4fb6be40203f21e2748
David Rafferty <drafferty@hs.uni-hamburg.de>
2019-04-23 09:53:48 +0200

    Add histogram and clean up

--- scripts/plot_uvcov.py
+++ scripts/plot_uvcov.py
@@ -10,6 +10,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy
 import casacore.tables as pt
+from matplotlib import cm
 
 
 def input2strlist_nomapfile(invar):
@@ -41,197 +42,179 @@ def string2bool(instring):
         raise ValueError('string2bool: Cannot convert string "'+instring+'" to boolean!')
 
 
-def main(input, output, title='uv coverage', limits=',,,', timeslots='0,0,0', antennas='-1', kilolambda=True,
-         markersize=2, wideband=True, sameuv=True, debug=False, flagged=True):
+def main(input, output, title='uv coverage', limits=',,,', timeslots='0,10,0', antennas='-1',
+         wideband=True):
 
-        debug = string2bool(debug)
         MSlist = input2strlist_nomapfile(input)
         if len(MSlist) == 0:
-                print 'Error: You must specify at least one MS name.'
-                sys.exit(1)
+            print 'Error: You must specify at least one MS name.'
+            sys.exit(1)
         plottitle = title
         fileformat = output.split('.')[-1]
         if fileformat not in ['png','pdf','eps','ps']:
-                print 'Error: Unknown file extension'
-                sys.exit(1)
+            print 'Error: Unknown file extension'
+            sys.exit(1)
         axlimits = limits.strip().split(',')
         if len(axlimits) == 4:
-                xmin,xmax,ymin,ymax = axlimits
+            xmin,xmax,ymin,ymax = axlimits
         else:
-                print 'Error: You must specify four axis limits'
-                sys.exit(1)
+            print 'Error: You must specify four axis limits'
+            sys.exit(1)
         timeslots = timeslots.split(',')
         if len(timeslots) != 3:
-                print 'Error: Timeslots format is start,skip,end'
-                sys.exit(1)
+            print 'Error: Timeslots format is start,skip,end'
+            sys.exit(1)
         for i in range(len(timeslots)):
-                timeslots[i] = int(timeslots[i])
-                if timeslots[i] < 0:
-                        print 'Error: timeslots values must not be negative'
-                        sys.exit(1)
+            timeslots[i] = int(timeslots[i])
+            if timeslots[i] < 0:
+                print 'Error: timeslots values must not be negative'
+                sys.exit(1)
         antToPlotSpl = antennas.split(',')
         antToPlot = []
         for i in range(len(antToPlotSpl)):
-                tmpspl = antToPlotSpl[i].split('..')
-                if len(tmpspl) == 1:
-                        antToPlot.append(int(antToPlotSpl[i]))
-                elif len(tmpspl) == 2:
-                        for j in range(int(tmpspl[0]),int(tmpspl[1])+1):
-                                antToPlot.append(j)
-                else:
-                        print 'Error: Could not understand antenna list.'
-                        sys.exit(1)
-        plotLambda = string2bool(kilolambda)
-        markerSize = int(markersize)
+            tmpspl = antToPlotSpl[i].split('..')
+            if len(tmpspl) == 1:
+                antToPlot.append(int(antToPlotSpl[i]))
+            elif len(tmpspl) == 2:
+                for j in range(int(tmpspl[0]),int(tmpspl[1])+1):
+                    antToPlot.append(j)
+            else:
+                print 'Error: Could not understand antenna list.'
+                sys.exit(1)
         wideband = string2bool(wideband)
-        sameuv = string2bool(sameuv)
-        flagged = string2bool(flagged)
 
         badval = 0.0
-        xaxisvals = numpy.array([])
-        yaxisvals = numpy.array([])
-        flagvals = numpy.array([], dtype=bool)
+        xaxisvals = []
+        yaxisvals = []
+        flagvals = []
         savex = numpy.array([])
         savey = numpy.array([])
         numPlotted = 0
         for inputMS in MSlist:
-                # open the main table and print some info about the MS
-                print 'Getting info for', inputMS
-                t = pt.table(inputMS, readonly=True, ack=False)
-                tfreq = pt.table(t.getkeyword('SPECTRAL_WINDOW'),readonly=True,ack=False)
-                ref_freq = tfreq.getcol('REF_FREQUENCY',nrow=1)[0]
-                ch_freq = tfreq.getcol('CHAN_FREQ',nrow=1)[0]
-                print 'Reference frequency:\t%f MHz' % (ref_freq/1.e6)
-                if wideband:
-                        ref_wavelength = 2.99792458e8/ch_freq
-                else:
-                        ref_wavelength = [2.99792458e8/ref_freq]
-                print 'Reference wavelength:\t%f m' % (ref_wavelength[0])
-                if sameuv and numPlotted > 0:
-                        print 'Assuming same uvw as first MS!'
-                        if plotLambda:
-                                for w in ref_wavelength:
-                                        xaxisvals = numpy.append(xaxisvals,[savex/w/1000.,-savex/w/1000.])
-                                        yaxisvals = numpy.append(yaxisvals,[savey/w/1000.,-savey/w/1000.])
-                        else:
-                                print 'Plotting more than one MS with same uv, all in kilometers... do you want -k?'
-                                xaxisvals = numpy.append(xaxisvals,[savex,-savex])
-                                yaxisvals = numpy.append(yaxisvals,[savey,-savey])
-                        if not flagged:
-                            continue
+            # open the main table and print some info about the MS
+            print 'Getting info for', inputMS
+            t = pt.table(inputMS, readonly=True, ack=False)
+            tfreq = pt.table(t.getkeyword('SPECTRAL_WINDOW'),readonly=True,ack=False)
+            ref_freq = tfreq.getcol('REF_FREQUENCY',nrow=1)[0]
+            ch_freq = tfreq.getcol('CHAN_FREQ',nrow=1)[0]
+            print 'Reference frequency:\t%f MHz' % (ref_freq/1.e6)
+            if wideband:
+                ref_wavelength = 2.99792458e8/ch_freq
+            else:
+                ref_wavelength = [2.99792458e8/ref_freq]
+            print 'Reference wavelength:\t%f m' % (ref_wavelength[0])
 
-                firstTime = t.getcell("TIME", 0)
-                lastTime = t.getcell("TIME", t.nrows()-1)
-                intTime = t.getcell("INTERVAL", 0)
-                print 'Integration time:\t%f sec' % (intTime)
-                nTimeslots = (lastTime - firstTime) / intTime
-                print 'Number of timeslots:\t%d' % (nTimeslots)
-                if timeslots[1] == 0:
-                        if nTimeslots >= 100:
-                                timeskip = int(nTimeslots/100)
-                        else:
-                                timeskip = 1
-                else:
-                        timeskip = int(timeslots[1])
-                print 'For each baseline, plotting one point every %d samples' % (timeskip)
-                if timeslots[2] == 0:
-                        timeslots[2] = nTimeslots
-                # open the antenna subtable
-                tant = pt.table(t.getkeyword('ANTENNA'), readonly=True, ack=False)
+            firstTime = t.getcell("TIME", 0)
+            lastTime = t.getcell("TIME", t.nrows()-1)
+            intTime = t.getcell("INTERVAL", 0)
+            nTimeslots = (lastTime - firstTime) / intTime
+            if timeslots[1] == 0:
+                    timeskip = 1
+            else:
+                    timeskip = int(timeslots[1])
+            if timeslots[2] == 0:
+                    timeslots[2] = nTimeslots
+            # open the antenna subtable
+            tant = pt.table(t.getkeyword('ANTENNA'), readonly=True, ack=False)
 
-                # Station names
-                antList = tant.getcol('NAME')
-                if len(antToPlot)==1 and antToPlot[0]==-1:
-                        antToPlot = range(len(antList))
+            # Station names
+            antList = tant.getcol('NAME')
+            if len(antToPlot)==1 and antToPlot[0]==-1:
+                    antToPlot = range(len(antList))
 
-                # select by time from the beginning, and only use specified antennas
-                tsel = t.query('TIME >= %f AND TIME <= %f AND ANTENNA1 IN %s AND ANTENNA2 IN %s' % (firstTime+timeslots[0]*intTime,firstTime+timeslots[2]*intTime,str(antToPlot),str(antToPlot)), columns='ANTENNA1,ANTENNA2,UVW,FLAG_ROW')
+            # select by time from the beginning, and only use specified antennas
+            tsel = t.query('TIME >= %f AND TIME <= %f AND ANTENNA1 IN %s AND ANTENNA2 IN %s' % (firstTime+timeslots[0]*intTime,firstTime+timeslots[2]*intTime,str(antToPlot),str(antToPlot)), columns='ANTENNA1,ANTENNA2,UVW,FLAG_ROW')
 
-                # Now we loop through the baselines
-                i = 0
-                nb = (len(antToPlot)*(len(antToPlot)-1))/2
-                for tpart in tsel.iter(["ANTENNA1","ANTENNA2"]):
-                        ant1 = tpart.getcell("ANTENNA1", 0)
-                        ant2 = tpart.getcell("ANTENNA2", 0)
-                        if ant1 not in antToPlot or ant2 not in antToPlot: continue
-                        if ant1 == ant2: continue
-                        i += 1
-                        # Get the values to plot
-                        if not sameuv or numPlotted == 0:
-                                uvw = tpart.getcol('UVW', rowincr=timeskip)
-                                savex = numpy.append(savex,[uvw[:,0],-uvw[:,0]])
-                                savey = numpy.append(savey,[uvw[:,1],-uvw[:,1]])
-                        if flagged:
-                            # Get the flags
-                            flags = tpart.getcol('FLAG_ROW', rowincr=timeskip)
-                        if plotLambda:
-                                for w in ref_wavelength:
-                                        if not sameuv:
-                                            xaxisvals = numpy.append(xaxisvals,[uvw[:,0]/w/1000.,-uvw[:,0]/w/1000.])
-                                            yaxisvals = numpy.append(yaxisvals,[uvw[:,1]/w/1000.,-uvw[:,1]/w/1000.])
-                                        if flagged:
-                                            flagvals = numpy.append(flagvals,[flags[:], flags[:]])
-                        else:
-                                if not sameuv:
-                                    xaxisvals = numpy.append(xaxisvals,[uvw[:,0]/1000.,-uvw[:,0]/1000.])
-                                    yaxisvals = numpy.append(yaxisvals,[uvw[:,1]/1000.,-uvw[:,1]/1000.])
-                                if flagged:
-                                    flagvals = numpy.append(flagvals,[flags[:], flags[:]])
-                numPlotted += 1
+            # Now we loop through the baselines
+            i = 0
+            nb = (len(antToPlot)*(len(antToPlot)-1))/2
+            for tpart in tsel.iter(["ANTENNA1","ANTENNA2"]):
+                ant1 = tpart.getcell("ANTENNA1", 0)
+                ant2 = tpart.getcell("ANTENNA2", 0)
+                if ant1 not in antToPlot or ant2 not in antToPlot: continue
+                if ant1 == ant2: continue
+                i += 1
+                # Get the values to plot
+                if numPlotted == 0:
+                    uvw = tpart.getcol('UVW', rowincr=timeskip)
+                    savex = numpy.append(savex,[uvw[:,0],-uvw[:,0]])
+                    savey = numpy.append(savey,[uvw[:,1],-uvw[:,1]])
 
+                # Get the flags
+                flags = tpart.getcol('FLAG_ROW', rowincr=timeskip)
+                for w in ref_wavelength:
+                    flagvals.extend(flags.tolist())
+                    flagvals.extend(flags.tolist())
+            for w in ref_wavelength:
+                xaxisvals.extend((savex/w/1000.).tolist())
+                yaxisvals.extend((savey/w/1000.).tolist())
+            numPlotted += 1
+
+        # Plot the uv coverage
         print 'Plotting uv points ...'
-
-        # Plot the data
-        if debug:
-                print xaxisvals
         xaxisvals = numpy.array(xaxisvals)
         yaxisvals = numpy.array(yaxisvals)
-        tmpvals = numpy.sqrt(xaxisvals**2+yaxisvals**2)
+        zvals = numpy.zeros(xaxisvals.shape)
+        flagvals = numpy.array(flagvals)
+        flagged_ind = numpy.where(flagvals)
+        zvals[flagged_ind] = 1.0
         uvmax = max(xaxisvals.max(),yaxisvals.max())
         uvmin = min(xaxisvals.min(),yaxisvals.min())
         uvuplim = 0.02*(uvmax-uvmin)+uvmax
         uvlolim = uvmin-0.02*(uvmax-uvmin)
         if xmin == '':
-                minx = uvlolim
+            minx = uvlolim
         else:
-                minx = float(xmin)
+            minx = float(xmin)
         if xmax == '':
-                maxx = uvuplim
+            maxx = uvuplim
         else:
-                maxx = float(xmax)
+            maxx = float(xmax)
         if ymin == '':
-                miny = uvlolim
+            miny = uvlolim
         else:
-                miny = float(ymin)
+            miny = float(ymin)
         if ymax == '':
-                maxy = uvuplim
+            maxy = uvuplim
         else:
-                maxy = float(ymax)
+            maxy = float(ymax)
         if minx == maxx:
-                minx = -1.0
-                maxx = 1.0
+            minx = -1.0
+            maxx = 1.0
         if miny == maxy:
-                miny = -1.0
-                maxy = 1.0
-        if plotLambda:
-                plt.xlabel(r'u [k$\lambda$]')
-                plt.ylabel(r'v [k$\lambda$]')
-                plt.xlim([minx,maxx])
-                plt.ylim([miny,maxy])
-        else:
-                plt.xlabel('u [km]')
-                plt.ylabel('v [km]')
-                plt.xlim([minx,maxx])
-                plt.ylim([miny,maxy])
-        plt.plot(xaxisvals[tmpvals!=badval], yaxisvals[tmpvals!=badval],'.',
-                 markersize=markerSize)
-        if flagged:
-            flagged_ind = numpy.where(numpy.logical_and(tmpvals!=badval, flagvals))
-            plt.plot(xaxisvals[flagged_ind], yaxisvals[flagged_ind],'.',
-                     markersize=markerSize, c='r')
+            miny = -1.0
+            maxy = 1.0
+        plt.xlabel(r'u [k$\lambda$]')
+        plt.ylabel(r'v [k$\lambda$]')
+        plt.xlim([minx,maxx])
+        plt.ylim([miny,maxy])
+        gridsize = 300
+        plt.hexbin(xaxisvals, yaxisvals, C=zvals, gridsize=gridsize, cmap=cm.jet, bins=None)
         plt.title(plottitle)
         plt.axes().set_aspect('equal')
         plt.grid(True)
-
+        cb = plt.colorbar()
+        cb.set_label('flagged fraction')
         plt.savefig(output)
+
+        # Plot histogram of the flagged fraction vs. uv distance
+        uvdist = numpy.sqrt(xaxisvals**2 + yaxisvals**2)
+        sorted_ind = numpy.argsort(uvdist)
+        uvdist = uvdist[sorted_ind]
+        zvals = 1.0-zvals[sorted_ind]
+        hist, bins = numpy.histogram(uvdist, 50)
+        bins_mean = [0.5 * (bins[i] + bins[i+1]) for i in range(len(bins)-1)]
+        inds = []
+        for b in bins[:-1]:
+            inds.append(numpy.where(uvdist > b)[0][0])
+        inds.append(len(bins))
+        flagged_mean = [numpy.mean(zvals[inds[i]:inds[i+1]]) for i in range(len(inds)-1)]
+        plt.clf()
+        plt.bar(bins_mean, flagged_mean, (bins[1]-bins[0])/2.0)
+        plt.xlabel(r'uv distance [k$\lambda$]')
+        plt.ylabel('unflagged fraction')
+        plt.savefig('{0}_uvdist.{1}'.format(*os.path.splitext(output)))
+
+
+
 
